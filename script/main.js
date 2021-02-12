03f38d463a0e741739ea1d786f6296984999e61b
Jsquad <lakis006@umn.edu>
2021-02-12 12:45:46 -0600

    finished targeting the buttons and adding some functionality to them.

--- script/main.js
+++ script/main.js
@@ -6,4 +6,31 @@ function addToDoItem() {
     alert("Add button clicked!!!")
 }
 
-console.log(addButton)
+
+
+var clearButton = document.querySelector("#clear-completed-btn") 
+
+clearButton.addEventListener("click", clearCompletedTodos); 
+function clearCompletedTodos() {
+    alert("Clear button clicked!!!")
+}
+
+
+
+var emptyButton = document.querySelector("#empty-list-btn") 
+
+emptyButton.addEventListener("click", emptyList); 
+function emptyList() {
+    alert("Empty list button clicked!!!")
+}
+
+
+
+var saveButton = document.querySelector("#save-btn") 
+
+saveButton.addEventListener("click", saveItemsList); 
+function saveItemsList() {
+    alert("Save button clicked!!!")
+}
+
+console.log(saveButton)
